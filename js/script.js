b870c3ed3b5920f11363e1267cc255984553d421
restu <restuwinaldi@gmail.com>
2024-04-27 15:01:18 +0700

    craft shop bag 1:resposive navbar

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Togle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
