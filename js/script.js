e30a6d3c73055940bbaf137bd783a374df550159
restu <restuwinaldi@gmail.com>
2024-05-02 10:54:54 +0700

    craft shop bag 6:view search form

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Togle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 // ketika hamburger menu di klik
@@ -6,11 +6,27 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
-// klik diluar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active serch
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
 
-document.addEventListener('click',function(e){
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+document.querySelector('#search-button').onclick = (e) => {
+    e.preventDefault();
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+}
+
+
+// klik diluar sidebar untuk menghilangkan nav
+const hb = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+
+document.addEventListener('click', function (e) {
+    if (!hb.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
     }
 })
