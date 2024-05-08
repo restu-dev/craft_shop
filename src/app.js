167cb69a8538cdda5e024dcf147b6ac3c8cef60f
restu <restuwinaldi@gmail.com>
2024-05-08 09:26:27 +0700

    craft shop bag :add fitur payment gateway midtrans

--- src/app.js
+++ src/app.js
@@ -40,36 +40,98 @@ document.addEventListener('alpine:init', () => {
                 });
             }
         },
-        remove(id){
+        remove(id) {
             // cek item yg akan di remve berdasarkan idnya
-            const cartItem = this.items.find((item)=> item.id ===id);
+            const cartItem = this.items.find((item) => item.id === id);
 
             // jika item lebih dari 1
-            if(cartItem.quantity>1){
+            if (cartItem.quantity > 1) {
                 // telusuri satu satu
-                this.items = this.items.map((item)=>{
+                this.items = this.items.map((item) => {
                     // jika bukan barang yang di klik
-                    if(item.id!==id){
+                    if (item.id !== id) {
                         return item;
-                    }else{
+                    } else {
                         item.quantity--;
-                        item.total = item.price*item.quantity;
+                        item.total = item.price * item.quantity;
                         this.quantity--;
-                        this.total -=item.price;
+                        this.total -= item.price;
                         return item;
                     }
                 })
-            } else if(cartItem.quantity===1){
+            } else if (cartItem.quantity === 1) {
                 // jika barangnya sisa 1
-                this.items = this.items.filter((item)=>item.id !==id);
+                this.items = this.items.filter((item) => item.id !== id);
                 this.quantity--;
-                this.total-=cartItem.price;
+                this.total -= cartItem.price;
 
             }
         }
 
     });
 });
+
+// Form validation
+const checkoutButton = document.querySelector('.checkoout-button');
+checkoutButton.disabled = true;
+
+const form = document.querySelector('#checkoutForm');
+
+form.addEventListener('keyup', function () {
+
+    for (let i = 0; i < form.elements.length; i++) {
+        if (form.elements[i].value.length !== 0) {
+            checkoutButton.classList.remove('disabled');
+            checkoutButton.classList.add('disabled');
+        } else {
+            return false;
+        }
+    }
+
+    checkoutButton.disabled = false;
+    checkoutButton.classList.remove('disabled');
+});
+
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener('click', async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const data = new URLSearchParams(formData);
+    const objData = Object.fromEntries(data);
+
+    // const message = formatMessage(objData);
+    // window.open('http://wa.me/6289669460382?text=' + encodeURIComponent(message));
+
+    // minta transaction token mengunakan ajax / fetch
+    try{
+        const response = await fetch('php/placeOrder.php',{
+            method:'POST',
+            body:data
+        });
+
+        const token = await response.text();
+        // console.log(token);
+        window.snap.pay(token);
+    }catch(err){
+        console.log(err.message)
+    }
+
+
+});
+
+// format pesan wa
+const formatMessage = (obj) => {
+    return `Data Customer
+        Nama ${obj.name}
+        Email ${obj.email}
+        No HP ${obj.phone}
+Data Pesanana
+    ${JSON.parse(obj.items).map((item) => `${item.name}(${item.quantity} x ${rupiah(item.total)}) \n`)}
+TOTAL: ${rupiah(obj.total)}
+Terima kasih.`;
+}
+
 
 // konversi ke rupiah
 const rupiah = (number) => {
